4e2e12268b8018cca5cce0cb1e918bbc6e3b97db
Student <student@bhavanas-computer.local>
2017-02-22 09:34:30 -0600

    Storyboard

---
+++ GreetMe/GreetingcardViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GreetingcardViewController.swift
+//  GreetMe
+//
+//  Created by Student on 2/22/17.
+//  Copyright © 2017 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class GreetingcardViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ GreetMe/InvitationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InvitationViewController.swift
+//  GreetMe
+//
+//  Created by Student on 2/22/17.
+//  Copyright © 2017 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class InvitationViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+}
