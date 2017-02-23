84a0cd2e750841b7caeefc3e918cbbbeb8b2a1b0
cstark <student@cstark.local>
2017-02-23 09:59:27 -0600

    change

--- GreetMe/GreetingcardViewController.swift
+++ GreetMe/GreetingcardViewController.swift
@@ -10,15 +10,11 @@ import UIKit
 
 class GreetingcardViewController: UIViewController
 {
-    @IBOutlet weak var nameOfrecepient: UITextField!
+    @IBOutlet weak var nameOfRecepient: UITextField!
     @IBOutlet weak var nameOfSender: UITextField!
     @IBOutlet weak var dateOfMessage: UITextField!
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myGreeting: UITextView!
-    var recepientName = UITextField.text
-    var senderName = UITextField.text
-    var messageDate = UITextField.text
-    var greeting = UITextView.text
     override func viewDidLoad()
     {
         super.viewDidLoad()
