19245942699603c5c3cdb923445ba11ff11b3334
cstark <student@cstark.local>
2017-02-27 09:58:44 -0600

    change

---
+++ GreetMe/GreetingDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingDetailViewController.swift
+//  GreetMe
+//
+//  Created by cstark on 2/27/17.
+//  Copyright © 2017 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class GreetingDetailViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
