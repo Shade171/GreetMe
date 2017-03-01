46a1590c755d37d55d8612d237f7c3e7e5b4277b
Student <student@bhavanas-computer.local>
2017-03-01 09:04:13 -0600

    Storyboard changes

---
+++ GreetMe/FinalViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FinalViewController.swift
+//  GreetMe
+//
+//  Created by Student on 2/28/17.
+//  Copyright © 2017 cstarkShade. All rights reserved.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
